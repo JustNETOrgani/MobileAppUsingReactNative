8616d61ecb0eddc482f2674e8b76c5dec2cde376
JustNETlab <justnetonline@outlook.com>
2021-04-01 17:55:31 +0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.appTitle}>Mobile App dev</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  appTitle: {
+    color: 'darkslateblue',
+    fontSize: 30,
+  },
+});
+
+export default App;

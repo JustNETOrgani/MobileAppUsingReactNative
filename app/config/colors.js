9398246ec92cf5400d54a7021ba39196d3544dc6
JustNETlab <justnetonline@outlook.com>
2021-04-03 18:40:39 +0800

    Colors updated

--- app/config/colors.js
+++ app/config/colors.js
@@ -1,7 +1,13 @@
 export default {
+  // Container
+  containerBackground: '#87CEEB',
+  // Header and Footer
   headerBackground: 'darkslateblue',
   footerBackground: '#87CEEB',
   // Text
   textHeader: '#fff',
   textFooter: '#fff',
+  textLoginGreeting: 'darkslateblue',
+  // Specific colors
+  fieldColors: 'darkslateblue',
 };
